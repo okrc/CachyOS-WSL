9f1e9fd4526a722ebe6b7eb39439ced2e1d576ff
okrc <okrc@hexo.dev>
2025-08-25 20:50:09 +0800

    Update oobe.sh
    Signed-off-by: okrc <okrc@hexo.dev>

--- linux_files/usr/lib/wsl/oobe.sh
+++ linux_files/usr/lib/wsl/oobe.sh
@@ -5,7 +5,8 @@ set -ue
 POWERSHELL_PATH=$(wslpath 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe')
 LANGUAGE_HOST=$($POWERSHELL_PATH -Command '(Get-Culture).Name' | tr -d '\r\n' | tr '-' '_')
 echo "Configuring locale for: ${LANGUAGE_HOST}.UTF-8"
-sed -i "s|^#\(${LANGUAGE_HOST}.UTF-8 UTF-8\).*|\1|g" /etc/locale.gen
+sed -i "s|^#[[:space:]]*\(en_US.UTF-8 UTF-8\).*|\1|g" /etc/locale.gen
+sed -i "s|^#[[:space:]]*\(${LANGUAGE_HOST}.UTF-8 UTF-8\).*|\1|g" /etc/locale.gen
 locale-gen
 localectl set-locale LANG=${LANGUAGE_HOST}.UTF-8
 export LANG=$(grep '^LANG=' /etc/locale.conf | cut -d'=' -f2)
